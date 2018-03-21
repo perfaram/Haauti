050a7af7fdf5c6292c2ff318bd1f00a371be80b9
Perceval FARAMAZ <perfaram@windowslive.com>
2018-03-21 11:18:22 +0100

    Initial Commit

---
+++ Haauti/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Haauti
+//
+//  Created by Perceval FARAMAZ on 21/03/2018.
+//  Copyright © 2018 deicoon. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Haauti/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Haauti
+//
+//  Created by Perceval FARAMAZ on 21/03/2018.
+//  Copyright © 2018 deicoon. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
